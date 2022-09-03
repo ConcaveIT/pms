9dfaa9c74c5a55ed8774d0643e1df5affce3cdf5
ConcaveIT <44179552+ConcaveIT@users.noreply.github.com>
2022-09-03 11:17:04 +0600

    update

--- resources/views/core/template/datatable_back/config/route.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$val .= "
-// Start Routes for ".$module->module_title." 
-Route::resource('{$route}',App\\Http\\Controllers\\".$controller."::class);
-// End Routes for ".$module->module_title."
-"; 
-
-?>                    
