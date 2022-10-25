c1d074b3a5cad89aed5327f09b122b29e344c25e
ConcaveIT <concaveit@gmail.com>
2022-10-26 03:14:03 +0600

    Update project-dashboard.blade.php

--- resources/views/project-dashboard.blade.php
+++ resources/views/project-dashboard.blade.php
@@ -409,13 +409,13 @@
 <script>
        var options = {
           series: [{
-          name: 'Net Profit',
+          name: 'Salary Disbursement',
           data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
         }, {
-          name: 'Revenue',
+          name: 'Cash Flow',
           data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
         }, {
-          name: 'Free Cash Flow',
+          name: 'Utility Expenses',
           data: [35, 41, 36, 26, 45, 48, 52, 53, 41,45,12,65]
         }],
           chart: {
